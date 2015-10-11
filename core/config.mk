zephiK/android_build

Fix global selinux policy inclusion We're adding specific policies to an overlay instead of patching external/sepolicy, so we need to make sure it's included to prevent misbehaving systems

core/config.mk
@@ -619,4 +619,8 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/chroma/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk

exdroid/build

core/config.mk
@@ -686,4 +686,8 @@ endif
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/xd/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk